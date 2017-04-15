57b86355a3c0361b7959b6218792be2e2473a68b
Mohamed Al-Rifay <mohamed.abdelattif@fci.helwan.edu.eg>
2017-04-15 22:49:56 +0200

    Add dis/connect event hander

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const express = require('express');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
+io.on('connection', (socket) => {
+    console.info("Client connected! :)");
+    socket.on('disconnect', () => {
+        console.warn('Disconnnected from client! :(');
+    });
+});
+
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     res.send('Hi :)');
-// });
-
-app.listen(port, (err) => {
+server.listen(port, (err) => {
     console.info(`Server is up on http://localhost:${port}`);
 });
